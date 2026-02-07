b7a49f0a856a4f3dde73a7414f7aa0b3af5f3961
haagamble <haagamble@gmail.com>
2026-02-07 23:03:37 +0200

    Add styles.css

--- .eleventy.js
+++ .eleventy.js
@@ -9,6 +9,10 @@ module.exports = function (eleventyConfig) {
     excerpt_separator: "<!-- excerpt -->"
   });
 
+  // Copy styles.css to the root of the output directory
+  eleventyConfig.addPassthroughCopy({ "src/styles.css": "styles.css" });
+
+
   return {
     dir: { input: "src", output: "_site" }
   };
